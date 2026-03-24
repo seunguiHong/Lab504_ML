7472196b7a8e31b6a5632c9eb2d265fa819d2695
홍승의 <seungui6518@gmail.com>
2026-03-24 14:12:18 +0900

    Remove files since project has been changed

--- B2B/Python/rolling_framework/__init__.py
+++
@@ -1,5 +0,0 @@
-# rolling_framework/__init__.py
-
-from .engine import ExpandingRunner
-from .strategies import make_strategy, Strategy
-from .sdmlp import MLPNet, SDMLPNet, TorchPlainMLPStrategy, TorchSDMLPStrategy
--- rolling_framework_1010/rolling_framework/__init__.py
+++
@@ -1,4 +0,0 @@
-# rolling_framework/__init__.py
-
-from .engine import ExpandingRunner
-from .strategies import make_strategy
